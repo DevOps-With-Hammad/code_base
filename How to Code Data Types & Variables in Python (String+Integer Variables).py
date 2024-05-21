9af6467d6f2b88b136988738bff99f4b5dd7b519
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-05-21 14:42:52 +0300

    Py Syntax error with Str in python simple

---
+++ How to Code Data Types & Variables in Python (String+Integer Variables).py
@@ -0,0 +1,29 @@
+# How to Code Data Types & Variables in Python (String+Integer Variables
+
+print("Hello world ")
+print('Hello world ')
+
+# print(str"Hello world ")
+"""
+SyntaxError: invalid syntax
+"""
+# Vars In python
+name = 'hammad'
+name1 = "hammad"
+# name3 = str'hammad'
+"""
+SyntaxError: invalid syntax
+"""
+#  name4 = str"hammad"
+"""
+SyntaxError: invalid syntax
+"""
+
+name5 = str("hammad ")
+# name6 = str(hamamd)
+"""
+SyntaxError: invalid syntax
+"""
+name7 = str('Hammad')
+print(name7)
+
