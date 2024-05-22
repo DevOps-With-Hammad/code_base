fd65981a79229df5d9c9f6535f293da5bb37629c
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-05-22 14:36:15 +0300

    # 2. [BIA] Arithmetic Rules in Python

---
+++ 3. [BIA] Level 3 Python Math (Meaning Operators) and Strings/2. [BIA] Arithmetic Rules in Python.py
@@ -0,0 +1,16 @@
+# 2. [BIA] Arithmetic Rules in Python
+
+# Let's practice some exercise
+# Ashoka is = 1.70 Meters
+# Yoda is = 0.66 Meters
+# R2D2 is = 1.09 Meters
+# C3P0 is = 1.75 Meters
+# print out the average  &&
+Ashoka = 1.023
+yoda = 0.66
+R2D2 = 1.09
+C3P0 = 1.75
+sum = float(yoda + Ashoka + R2D2 + C3P0)
+print(sum)
+Aveg = float(sum) / 4
+print(f"The Aveg is :\n {Aveg}")
