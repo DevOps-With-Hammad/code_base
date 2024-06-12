342c031a65b4e18f1273a0f9d9d113e36cb55f93
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-06-12 03:09:50 +0300

    14 advanced def function to return a written name or any empty name that already setted into the function

---
+++ 14. [IGA] Creating Advanced Functions (Keyword and Default Values).py
@@ -0,0 +1,10 @@
+# 14. [IGA] Creating Advanced Functions (Keyword and Default Values)
+def your_name(Name= "Yooda"):
+    print(f"The Name is {Name}")
+
+
+your_name("Hammad Ibrahim")
+your_name()
+your_name("Ahmed Slaeh ")
+your_name()
+your_name("ELsam slaam")
