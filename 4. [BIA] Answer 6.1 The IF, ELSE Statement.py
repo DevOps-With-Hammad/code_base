485ceb1d4047493bb4353e0dc3a6e3605c4a73f2
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-07-04 16:15:57 +0300

    4. [BIA] Answer 6.1 The IF, ELSE Statement>> if statment simple way

---
+++ 4. [BIA] Answer 6.1 The IF, ELSE Statement.py
@@ -0,0 +1,6 @@
+# 4. [BIA] Answer 6.1 The IF, ELSE Statement
+question = input("Is Rose holding a lightsaber ??")
+if question == "yes":
+    print("Yes she is holding a lightsaber")
+else:
+    print("she is not holding anything at all ")
