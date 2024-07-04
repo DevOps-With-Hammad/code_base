393f898a8eab7c939426520461ce42621f6a3e47
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-07-04 16:57:07 +0300

    # 8. [BIA] Using ELIF in an IF Statement and the OR & AND Logic

---
+++ 8. [BIA] Using ELIF in an IF Statement and the OR & AND Logic.py
@@ -0,0 +1,9 @@
+# 8. [BIA] Using ELIF in an IF Statement and the OR & AND Logic
+army_size =input("what is the army size 1 , 2 or 3 ")
+if army_size == "1":
+    print("Yes army 1 will beat form 2 and 3 ")
+elif army_size == "2":
+    print(" cool the army 2 will beat army 1 and got beated form 3")
+
+else:
+    print(" you've got it right ..  ")
