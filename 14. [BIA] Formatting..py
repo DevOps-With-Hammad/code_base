e83d6ab5fe20256b96a08aba2b38d22e7fd8821e
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-08-31 00:42:06 +0300

    Formating

---
+++ 14. [BIA] Formatting..py
@@ -0,0 +1,12 @@
+# 14. [BIA] Formatting.
+
+ticket = input("what is you choice between 1 /2 or 3 ")
+print(f"you ticket number is {ticket}")
+
+if ticket == "1":
+    price = 365.5
+else:
+    price = 650.4
+
+print(f"you've selected {ticket}\n with the price {(price)}")
+# Formating in python
