8357409d6543f5746eb557ac8252f5e9b8244112
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-08-31 23:38:14 +0300

    [BIA] Exercise 6.6 Slicing

---
+++ 18. [BIA] Exercise 6.6 Slicing.py
@@ -0,0 +1,11 @@
+# 18. [BIA] Exercise 6.6 Slicing
+"""
+creating a variable (an object ) called Titanic
+Then create 3 lines of code that use 3 ways to print the letter  c from  Titanic slicing
+using Three slicing methodologies ( one for each line of code )
+
+"""
+
+Full_Name = "Software"
+
+print(Full_Name[2], Full_Name[0:2], Full_Name[2:3])
