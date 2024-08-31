89bb3b6304d71c8abbcb8f0324e152deb95c9685
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-08-31 15:24:18 +0300

    Full_Name[-8]

---
+++ 17. [BIA] Slicing.py
@@ -0,0 +1,12 @@
+# 17. [BIA] Slicing
+"""
+Create a variable (an object called Hammad DevOps
+Then create 3 lines of code that use 3 ways to print the letter d from Hammad DevOps using slicing
+Three slicing methodologies
+
+"""
+Full_Name = "Hammad DevOps"
+print(Full_Name[5])
+print(Full_Name[-8])
+print(Full_Name[5:6])
+print(Full_Name[7:8], Full_Name[-8:-7], Full_Name[5:6], Full_Name[-8])
