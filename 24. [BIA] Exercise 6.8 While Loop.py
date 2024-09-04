d441e472be1361c23201e9cf221492ec49f98878
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-09-05 02:38:17 +0300

    [BIA] Excercise 6.8 while loop

---
+++ 24. [BIA] Exercise 6.8 While Loop.py
@@ -0,0 +1,26 @@
+# 24. [BIA] Exercise 6.8 While Loop
+"""
+Create  a while loop print this :-
+
+1 the year is 1901 >> This is the year when nobody found Titanic yet .
+1 the year is 1902 >> This is the year when nobody found Titanic yet .
+1 the year is 1903 >> This is the year when nobody found Titanic yet .
+
+The year is 1984 when titanic found.
+
+Kids = 50
+while Kids <= 60:
+    print(f" coool{Kids}")
+    Kids = Kids +1
+
+"""
+Year_counter = 1912
+
+while Year_counter <= 1984:
+    print(f"The year is {Year_counter} this is not the year when nobody found titanic yet ")
+    if Year_counter == 1984:
+        print(f"This is the year where tictinc where found  {Year_counter}")
+    Year_counter = Year_counter + 1
+
+
+
