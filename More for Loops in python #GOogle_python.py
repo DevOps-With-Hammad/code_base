c7d043817edd17387978012a31d9e0c0b5ae8736
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-09-13 04:12:30 +0300

    Nested for loop

---
+++ More for Loops in python #GOogle_python.py
@@ -0,0 +1,18 @@
+for i in [2,8,9,10]:
+    print("access denied ")
+print("This only prints 4 elements we need 5 ")
+# this only prints 4 elements
+
+for i in range(1,7):
+    print("Access denied ")
+    # this only prints ^6  times we need 5 times
+print("We need only 5 times this prints 6 times \n can you believed  it man ")
+
+
+# let's  try this one
+# for i in range[5]:
+  #  print("Access denied ")
+
+  # this is going to print an error in the language syntax .
+  
+
---
+++ Nested for loops.py
@@ -0,0 +1,5 @@
+# Nested for loops
+for x in range(10):
+    for y in range(x,10):
+        print(f"[{x} | {y} ]" ,end=" ")
+    print()
