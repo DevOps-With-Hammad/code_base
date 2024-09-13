67ddc8be9567b31302ce1059aba698786a0b7a4d
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-09-13 14:34:54 +0300

    Google python Sclicing

---
+++ How to Slice Strings.py
@@ -0,0 +1,7 @@
+# Slicing and joining
+
+string1 = "Greetings, Earthlings "
+print(string1[0]) # prints "G"
+print(string1[4:8]) # prints "ting"
+print(string1[11:]) # prints "Earthlings"
+print(string1[:5]) # print "greet"
