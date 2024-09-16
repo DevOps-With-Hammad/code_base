3c465792eec3ce316f50201528675797c6d589f3
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-09-16 04:16:28 +0300

    problem solved

---
+++ print even number using for loop in form 0 to 18.py
@@ -0,0 +1,13 @@
+for n in range (19):
+    if n % 2 == 0:
+        print(n)
+
+
+# for x in range(0,18+1,2):
+  #   print(n)
+
+# for x in range(18+1):
+  #   print(x **2)
+
+for m in range(10):
+    print(mm)
