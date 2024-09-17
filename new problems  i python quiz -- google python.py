5de90cbb5c88117473651bb02a42a6178495af55
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-09-18 00:38:14 +0300

    for loop extracting odd numbers in range of 0 to a given value  GoOgle python quiz

--- new problems  i python quiz -- google python.py
+++ new problems  i python quiz -- google python.py
@@ -7,3 +7,46 @@ def count_to_ten():
 
 
 count_to_ten()
+
+#######################
+def sequence(low, high) :
+  # Outer loop to run twice
+  for x in range(2) :  # Runs twice to create two rows
+    # Inner loop to print from 'high' to 'low'
+    for y in range(high, low - 1, -1) :  # Decrementing from 'high' to 'low'
+      if y == low :
+        # Don’t print a comma after the last item
+        print(str(y))
+      else :
+        # Print a comma and a space between numbers
+        print(str(y), end=", ")
+    # Print a newline after each row
+    print()
+
+
+sequence(1, 3)
+
+
+#############################
+def odd_numbers(maximum) :
+  return_string = ""  # Initializes variable as a string
+
+  # Complete the for loop with a range that includes all
+  # odd numbers up to and including the "maximum" value.
+  for x in range(maximum + 1) :
+    # Complete the body of the loop by appending the odd number
+    # followed by a space to the "return_string" variable.
+    if x % 2 != 0 :
+      print(x, end=" ")
+
+  # This .strip command will remove the final " " space
+  # at the end of the "return_string".
+  return return_string.strip()
+
+
+print(odd_numbers(6))  # Should be 1 3 5
+print(odd_numbers(10))  # Should be 1 3 5 7 9
+print(odd_numbers(1))  # Should be 1
+print(odd_numbers(3))  # Should be 1 3
+print(odd_numbers(0))  # No numbers displayed
+
