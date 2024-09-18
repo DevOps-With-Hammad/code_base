1d800f233bd363bf6f1f4e4bb94fab63b42f029f
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-09-18 05:07:52 +0300

    finding the error of this infinit loop by incrementing the x var by one

--- new problems  i python quiz -- google python.py
+++ new problems  i python quiz -- google python.py
@@ -58,3 +58,15 @@ while x <= 10:
     x = x+1
 print(sum)
 
+########################
+#
+#
+def count_to_ten():
+  # Loop through the numbers from first to last
+  x = 1
+  while x <= 10:
+    print(x)
+    x += 1
+
+
+count_to_ten()
